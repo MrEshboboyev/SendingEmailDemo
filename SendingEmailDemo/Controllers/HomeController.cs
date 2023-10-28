013c1924f4900997e64f7127a3edf48a8c067635
Diyorbek Eshboboyev <mrdiyorbekeshboboyev@gmail.com>
2023-10-28 10:36:53 +0500

    Project Finished

--- SendingEmailDemo/Controllers/HomeController.cs
+++ SendingEmailDemo/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SendingEmailDemo.Models;
 using System.Diagnostics;
+using System.Net.Mail;
 
 namespace SendingEmailDemo.Controllers
 {
@@ -27,6 +28,51 @@ namespace SendingEmailDemo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SendEmail (EmailEntity objEmailParameters, 
+            IFormFile PostedFile)
+        {
+            var myAppConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var Username = myAppConfig.GetValue<string>("EmailConfig:Username");
+            var Password = myAppConfig.GetValue<string>("EmailConfig:Password");
+            var Host = myAppConfig.GetValue<string>("EmailConfig:Host");
+            var Port = myAppConfig.GetValue<int>("EmailConfig:Port");
+            var FromEmail = myAppConfig.GetValue<string>("EmailConfig:FromEmail");
+
+            MailMessage message = new MailMessage();
+            message.From =  new MailAddress(FromEmail);
+            message.To.Add(objEmailParameters.ToEmailAddress.ToString());
+            message.Subject = objEmailParameters.Subject;
+            message.IsBodyHtml = true;
+            message.Body = objEmailParameters.EmailBodyMessage;
+
+
+            message.Attachments.Add(new Attachment(PostedFile.OpenReadStream(),
+                PostedFile.FileName));
+
+            SmtpClient mailClient = new SmtpClient();
+            try
+            {
+                mailClient.UseDefaultCredentials = false;
+                mailClient.Credentials = new System.Net.NetworkCredential(Username, Password);
+                mailClient.Host = Host;
+                mailClient.Port = Port;
+                mailClient.EnableSsl = false;
+                mailClient.Send(message);
+                ViewBag.Message = "Email Sent Successfully!!!";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Failed Sending Email";
+            }
+            finally
+            {
+                mailClient.Dispose();
+            }
+
+            return View("Index");
         }
     }
 }
--- SendingEmailDemo/Models/EmailEntity.cs
+++ SendingEmailDemo/Models/EmailEntity.cs
@@ -1,6 +1,19 @@
-﻿namespace SendingEmailDemo.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace SendingEmailDemo.Models
 {
     public class EmailEntity
     {
+        [ValidateNever]
+        public string FromEmailAddress { get; set; }
+
+        public string ToEmailAddress { get; set; }
+
+        public string Subject { get; set; }
+
+        public string EmailBodyMessage { get; set; }
+
+        [ValidateNever]
+        public string AttachmentURL { get; set; }
     }
 }
